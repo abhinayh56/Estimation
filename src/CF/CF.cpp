d199cee12a95530ed67ee988c5ef88ce1b54b3ec
abhinayh56 <kumarabhinay53@gmail.com>
2025-06-23 19:46:51 +0530

    asdfsafd

--- src/CF/CF.cpp
+++ src/CF/CF.cpp
@@ -11,15 +11,18 @@ CF<T>::~CF()
 }
 
 template <typename T>
-void CF<T>::init(T alpha_)
+void CF<T>::init(T fc_, T dt_)
 {
-    alpha = alpha_;
+    set_param(fc_, dt_);
 }
 
 template <typename T>
-void CF<T>::set_param(T alpha_)
+void CF<T>::set_param(T fc_, T dt_)
 {
-    alpha = alpha_;
+    fc = fc_;
+    dt = dt_;
+    tau = 1.0 / (MATH_2_PI * fc);
+    alpha = dt / (dt + tau);
 }
 
 template <typename T>
@@ -31,13 +34,52 @@ T CF<T>::update(T x1_i, T x2_i)
 template <typename T>
 void CF<T>::reset()
 {
-    y_i = 0.0;
+}
+
+template <typename T>
+void CF<T>::set_fc(T fc_)
+{
+    set_param(fc_, dt);
+}
+
+template <typename T>
+void CF<T>::set_tau(T tau_)
+{
+    tau = tau_;
+    fc = 1.0 / (2.0 * MATH_PI * tau);
+    alpha = dt / (dt + tau);
+}
+
+template <typename T>
+void CF<T>::set_dt(T dt_)
+{
+    set_param(fc, dt_);
 }
 
 template <typename T>
 void CF<T>::set_alpha(T alpha_)
 {
     alpha = alpha_;
+    tau = ((1.0 - alpha) * dt) / alpha;
+    fc = 1.0 / (2.0 * MATH_PI * tau);
+}
+
+template <typename T>
+T CF<T>::get_fc()
+{
+    return fc;
+}
+
+template <typename T>
+T CF<T>::get_tau()
+{
+    return tau;
+}
+
+template <typename T>
+T CF<T>::get_dt()
+{
+    return dt;
 }
 
 template <typename T>
